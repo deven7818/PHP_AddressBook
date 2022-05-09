e1a83e2dd5e280b72977e6053f62fbf9f06a85f1
deven7818 <deven7818@gmail.com>
2022-05-09 12:15:12 +0530

    Added : Created Contact in address Book

--- AddressBook.php
+++ AddressBook.php
@@ -1,8 +1,22 @@
 <?php
 
+/**
+ * Uc-1
+ * Create contact in address book with following details
+ * - first name, last name, address, city, state, zip, phone number and email.
+ */
 
 class AddressBook
 {
+    public $firstName;
+    public $lastName;
+    public $address;
+    public $city;
+    public $state;
+    public $zip;
+    public $phoneNumber;
+    public $email;
+
 
     /**
      * Function for printing Welcome message
@@ -12,8 +26,40 @@ class AddressBook
         echo "Welcome to Address Book Program\n";
     }
 
+    /**
+     * Function to get contact details information from user
+     */
+    function getContactDetails()
+    {
+        $this->firstName = readline("Enter your first name : ");
+        $this->lastName = readline("Enter your last name : ");
+        $this->address = readline("Enter your address : ");
+        $this->city = readline("Enter your city : ");
+        $this->state = readline("Enter your state : ");
+        $this->zip = readline("Enter your zip code : ");
+        $this->phoneNumber = readline("Enter your phone Number : ");
+        $this->email = readline("Enter your email : ");
+    }
+
+    /**
+     * Function to show contact information given by user
+     */
+    function showContactDetails()
+    {
+        echo "Contact information is : ";
+        echo "\nFirst Name : " . $this->firstName;
+        echo "\nLast Name : " . $this->lastName;
+        echo "\nAddress : " . $this->address;
+        echo "\nCity : " . $this->city;
+        echo "\nState : " . $this->state;
+        echo "\nZip Code : " . $this->zip;
+        echo "\nPhone Number : " . $this->phoneNumber;
+        echo "\nEmail address : " . $this->email;
+    }
 }
 //calling functions
 $addressBook = new AddressBook();
 $addressBook->welcome();
+$addressBook->getContactDetails();
+$addressBook->showContactDetails();
 ?>
